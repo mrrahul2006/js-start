b88f267a6dbf65f74e8defee5f1358482d199ebf
mrrahul2006 <2403051050376@paruluniversity.ac.in>
2026-06-15 13:21:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //trats all js code as newer version
+// alert(3+3) we are using node.js not browser
+
+console.log(3+3);//code readability should be high
+
+
+
+let  age=78//integer type
+let name="kumar"//string
+let isloggedIn=false//boolean
+
+
+//#Primitive datatypes
+
+//number=>2 to power 53
+//bigint =>normally not but used it for big data ypes
+//String =>we can use it in both" " or '  ' 
+//boolean=> true or false
+//null=>standalone value
+//undefined => 
+//symbol => for  uniqueness
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
