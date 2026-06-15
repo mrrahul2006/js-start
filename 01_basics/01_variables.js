ba0a4faecd70d08dda0a0e2bf907b0b0c34194f9
mrrahul2006 <2403051050376@paruluniversity.ac.in>
2026-06-15 13:22:40 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const acccountId=873495
+let accountEmail="kumarrahul@3008"
+var accountPassword="132adv"
+accountCity="jaipur"
+
+// acccountId=2  
+// //Not allowed in javasript
+console.log(acccountId);
+
+accountEmail="kumar@3334"
+accountPassword="qwe22223"
+accountCity="patna"
+console.table([acccountId,accountCity,accountEmail,accountPassword])
