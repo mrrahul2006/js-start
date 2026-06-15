9df0f47e08df09a749a766cc9c32e70818bb88ca
mrrahul2006 <2403051050376@paruluniversity.ac.in>
2026-06-15 16:19:50 +0000

    coversion completed

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,27 @@
+let score="53ah"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)//print nan
+
+// "33"=>33
+// "33am"=>Nan
+// true => 1;false=>0
+
+let isloggedIn="uu"
+
+let booleanIsLoggedin = Boolean(isloggedIn)
+console.log(booleanIsLoggedin)
+
+//1=> true ;0=>false
+//""=>false
+//"kumar"=>true
+
+let n=20
+
+let b= String(n)
+console.log(b)
+console.log(typeof b)
