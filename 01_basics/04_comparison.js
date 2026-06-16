1ced34ecd0c0b2ce74c5f4d1860446e27c369e28
mrrahul2006 <2403051050376@paruluniversity.ac.in>
2026-06-16 12:26:13 +0000

    comparison completed

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+console.log(2>5)
+console.log(3>=4)
+console.log(4==4)
+
+// comparison with different datatypes
+
+console.log("5">2)
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null==undefined)
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+ //strict 
+
+// console.log(null===0)
