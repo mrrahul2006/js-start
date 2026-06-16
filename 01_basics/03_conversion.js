a9b1fc530e2ae8b94b7e5a08acaa1b3b867755a5
mrrahul2006 <2403051050376@paruluniversity.ac.in>
2026-06-16 12:25:49 +0000

    operation added

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -25,3 +25,30 @@ let n=20
 let b= String(n)
 console.log(b)
 console.log(typeof b)
+//***************  Operations  ****************** */
+
+let value=4
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="rahul"
+let str2="ratnakar"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+3)//13
+console.log(1+"4")//14
+console.log("1"+3+4)//134
+console.log(2+3+4+"5")//95
+console.log(typeof (null+3+4+"4"))//string
+console.log(true+4)//5
+console.log(typeof true)//number
+// console.log(true+) error
+
