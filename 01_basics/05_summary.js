3dacb470eb239e3b9fddf64286891e00d99394eb
mrrahul2006 <2403051050376@paruluniversity.ac.in>
2026-06-16 12:26:36 +0000

    summary of datatypes

---
+++ 01_basics/05_summary.js
@@ -0,0 +1,30 @@
+// # primitive data type
+//7 types  : String,number,boolean , null , undefined,Symbol , BigInt
+
+const score=100
+const sc=99.99
+
+const isloggedIn=false
+// Symbol declaration
+const id=Symbol('kumar3444')
+const anotherId=Symbol('kumar3444')
+console.log(id)
+console.log(anotherId)
+console.log(id===anotherId)
+// javascript is dynamir\cally typed language
+
+
+
+//# reference (non-premitive)type
+// Array , objects , Functions
+const heroes = ["shaktiman","naagraj","doga"]
+// obj
+let myobj={
+    name="kumar",
+    age=90
+}
+
+// function
+const myfunct=function(){
+    console.log("Hello future coder")
+}
