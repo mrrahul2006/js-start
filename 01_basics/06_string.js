f80b3fb41dd2dbd76674b1c3bf3ea5f2b5efa246
mrrahul2006 <2403051050376@paruluniversity.ac.in>
2026-06-17 14:53:06 +0000

    string completed

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -14,3 +14,27 @@ console.log(gameName.__proto__)
 console.log(gameName.length)
 console.log(gameName.toUpperCase())
 console.log(gameName.charAt(2))
+console.log(gameName.indexOf('k'))
+
+const newString = gameName.substring(0,4);
+console.log(newString)
+
+// in slice method we cn use negative values also positive negative will print from last index
+const anotherString = gameName.slice(-6,4)
+console.log(anotherString)
+
+// .trim() for space removal
+const nname = "    kumar   "
+console.log(nname.trim())
+
+// .replace() any char with any char
+const url = "kuma--rrahul--@gmail.com"
+const murl=url.replaceAll("-","")
+console.log(murl)
+
+// .includes() method search that word is in the 
+console.log(url.includes(`kumar`))
+
+// .split()
+
+console.log(url.split('-'))
