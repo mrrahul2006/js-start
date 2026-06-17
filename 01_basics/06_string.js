f555f95094cdc810c657d8cad1a93f8d758807e4
mrrahul2006 <2403051050376@paruluniversity.ac.in>
2026-06-17 08:06:15 +0000

    string under process

---
+++ 01_basics/06_string.js
@@ -0,0 +1,16 @@
+const name = "rahul"
+const repo =50
+// console.log(name+repo+"value");
+
+// backticks => ``
+
+console.log(`hello my name is ${name} and my repo is ${repo}`)
+
+// another declaration of string
+
+const gameName = new String(`ratnakar`)
+console.log(gameName[4])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
