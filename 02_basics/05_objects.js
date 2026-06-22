8173dc79964974d83bbabe2ff0300ad6a40701c2
mrrahul2006 <2403051050376@paruluniversity.ac.in>
2026-06-22 17:22:34 +0000

    object done

--- 02_basics/05_objects.js
+++ 02_basics/05_objects.js
@@ -1,1 +1,29 @@
 // Hidden classes :- javascript is dynamic , but V8 internally creates hidden classes to optimize object access
+// const tinderuser = new Object();
+const tinderuser = {}
+tinderuser.id="123abc"
+tinderuser.name="sammy"
+tinderuser.isLoggedIn=false
+console.log(tinderuser)
+
+const user={
+    name:"ratnakar",
+    department:"it",
+    skills:[
+        "html",
+        "css"
+    ]
+}
+
+
+//***************API's************** */
+// {
+//     "name":"ratnakar",
+//     "coursename":"hindi",
+//     "price":"free"
+// }
+[
+    {},
+    {},
+    {}
+]
